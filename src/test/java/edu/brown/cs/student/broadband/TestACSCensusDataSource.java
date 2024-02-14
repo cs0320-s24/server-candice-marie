d0a6ffcf27560244f89c225b68b8bd8725a728ae
Candice Cai <caixy.candice2016@outlook.com>
2024-02-14 15:01:55 -0800

    test: more tests for getbroadband

--- src/test/java/edu/brown/cs/student/broadband/TestACSCensusDataSource.java
+++ src/test/java/edu/brown/cs/student/broadband/TestACSCensusDataSource.java
@@ -1,10 +1,14 @@
 package edu.brown.cs.student.broadband;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 import edu.brown.cs.student.main.broadband.ACSCensusDataSource;
+import edu.brown.cs.student.main.broadband.exceptions.DataNotFoundException;
 import edu.brown.cs.student.main.broadband.exceptions.DataSourceException;
+import edu.brown.cs.student.main.broadband.exceptions.InputNotFoundException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,10 +37,12 @@ public class TestACSCensusDataSource {
     // Verify statecode_map is populated correctly
     assertEquals("01", dataSource.getStatecode_map().get("Alabama"));
   }
+
   @Test
   public void testGetCountyCode() throws IOException, DataSourceException {
     when(mockConnection.getResponseCode()).thenReturn(200);
-    InputStream inputStream = new ByteArrayInputStream("[[\"Autauga County, Alabama\",\"01\",\"001\"]]".getBytes());
+    InputStream inputStream =
+        new ByteArrayInputStream("[[\"Autauga County, Alabama\",\"01\",\"001\"]]".getBytes());
     when(mockConnection.getInputStream()).thenReturn(inputStream);
     dataSource.getCountyCode("01");
     // Verify statecode_map is populated correctly
@@ -44,7 +50,17 @@ public class TestACSCensusDataSource {
   }
 
   @Test
-  public void testGetBroadBandPercentage() {
-
+  public void testGetBroadBandPercentage() throws DataNotFoundException, DataSourceException {
+    dataSource.getStateCode();
+    assertNotNull(dataSource.getBroadbandPercentage("Elmore County", "Alabama"));
+    assertThrows(
+        DataNotFoundException.class,
+        () -> dataSource.getBroadbandPercentage("Autauga County", "Alabama"));
+    assertThrows(
+        InputNotFoundException.class,
+        () -> dataSource.getBroadbandPercentage("Autauga County", "xxx"));
+    assertThrows(
+        InputNotFoundException.class,
+        () -> dataSource.getBroadbandPercentage("Autauga xxx", "Alabama"));
   }
 }
